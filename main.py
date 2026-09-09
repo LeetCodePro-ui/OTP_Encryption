0d38a66b8170e609537521132c20ba2f38877870
LeetCodePro-ui <herhoffer.marco@gmail.com>
2026-09-09 17:51:49 +0000

    Cleaned code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import secrets
-
+import sys
 
 banner = r'''
    ______  ___________  _______    __    _______  _____  ___    ______    _______   ___  ___  _______   
@@ -16,20 +16,26 @@ print(banner)
 print("\n\n")
 
 def encrypt(i,key):
+     #1:turning character i into ASCII number
+     #2:Subtracting 32, adding key
+     #3:mod 95 to receive number in ASCII scale, adding 32 to not leave printable character scale
     return chr((ord(i) - 32 +key) % 95 + 32)
 
 while True:
     try:
         while True:
-            choice = input("Please enter your choice:\n1:Encryption\n")
+            choice = input("Please enter your choice:\n1:END Program\n2:Encryption\n")
             if choice == str(1):
+                print("\n--- Program finished ---")
+                sys.exit()
+            elif choice == str(2):
                 break
             else:
                 print("ERROR\nEnter a valid option!")
                 continue
 
 
-        if choice == str(1):
+        if choice == str(2):
             txt = input("Enter your text for encryption here.\n")
 
 
@@ -42,16 +48,13 @@ while True:
             for i in txt:
             #secret number until 95 because of printable ASCII characters
                 key = secrets.randbelow(95)
-                key_list.append(key)
-                #1:turning character i into ASCII number
-                #2:Subtracting 32, adding key
-                #3:mod 95 to receive number in ASCII scale, adding 32 to not leave printable character scale
+                key_list.append(key)        
                 enc_txt += encrypt(i,key)
             print(enc_txt)
             print(key_list)
 
     except KeyboardInterrupt:
-        print("\n--- KeyboardInterrupt---")
+        print("\n--- KeyboardInterrupt ---")
         print("\n--- Program finished ---")
         break
 
